4976421cefce5005303308f16b00d21b262701d9
Mingzi Cao <mingzicao0407@163.com>
2018-03-22 17:20:06 -0400

    Main.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from './Header'
-
+import { Main } from  './Main'
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main/>
       </div>
     );
   }
